6e5c8281d06fd4d8855901e5e93210c99cd89bf5
Mutsuki Kimuraya <zaubermaerchen@me.com>
2022-09-19 10:36:54 +0900

    add firebase setting

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,3 +5,16 @@ declare module '*.vue' {
   const component: DefineComponent<Record<string,unknown>, Record<string,unknown>, unknown>;
   export default component;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_API_KEY: string
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string
+  readonly VITE_FIREBASE_PROJECT_ID: string
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string
+  readonly VITE_FIREBASE_APP_ID: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
